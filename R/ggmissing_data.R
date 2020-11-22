879042be54821f2f0d516fbd81757f4d4ebc3957
jmoggridge <ja.moggridge@gmail.com>
2020-11-22 02:16:04 -0500

    trying to figure out how to fix non-standard evaluation issues with plyr

--- R/ggmissing_data.R
+++ R/ggmissing_data.R
@@ -5,31 +5,36 @@
 #'
 #' @return ggplot object
 #' @export
-#' @importFrom dplyr mutate arrange
+#' @importFrom ggplot2 ggplot geom_col labs theme_light
+#' @importFrom dplyr mutate arrange everything
 #' @importFrom forcats as_factor
 #' @importFrom tidyr pivot_longer
 #' @importFrom purrr map2_dfc
 #' @importFrom magrittr %>%
-#' @importFrom ggplot2 ggplot geom_col labs theme_light
+#' @importFrom rlang .data
 
 gg_missing <- function(df=df) {
 
   ## plots % of missing values for each variable in a dataframe
   # count missing values in each column;
   purrr::map2_dfc(.x=df, .y=names(df), .f=~sum(is.na(.))) %>%
-    tidyr::pivot_longer(everything()) %>%
+    tidyr::pivot_longer(dplyr::everything()) %>%
     # sort vars descending by % NA and make barplot
     dplyr::arrange(value) %>%
     dplyr::mutate(value = value/nrow(df),
            name = forcats::as_factor(name)) %>%
     # make a bar plot
-    ggplot2::ggplot(aes(y = name, x = value)) +
-    geom_col() +
-    theme_light() +
-    labs(
+    ggplot2::ggplot(
+      ggplot2::aes(y = .data$name, x = .data$value)
+      ) +
+    ggplot2::geom_col() +
+    ggplot2::theme_light() +
+    ggplot2::labs(
       x = '% NA values',
       y = 'Variable',
-      title = paste0('Missing data in dataframe:\n"', deparse(substitute(df)), '"')
+      title = paste0('Missing data in dataframe:\n"',
+                     deparse(substitute(df)), '"')
     )
 }
 
+gg_missing(iris)
